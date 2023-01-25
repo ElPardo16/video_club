fe5d44c15943bbfbefee8dade2b62b5f7f4d9570
josesunrise <114879421+mariasuescumg@users.noreply.github.com>
2023-01-25 15:51:32 -0500

    connect

---
+++ src/db/connection.js
@@ -0,0 +1,7 @@
+import { connect } from "mongoose";
+
+export default  function conn(){
+    connect(process.env.URL_DB)
+    .then(_ => {console.log("connect")} )
+    .catch(err =>{console.log("error:" + err)} )
+}
