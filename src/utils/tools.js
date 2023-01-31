759be7c372aef8d699b2ed8d8fc41f610f2366d4
ElPardo16 <andresito105@gmail.com>
2023-01-31 13:03:46 -0500

    changue links for production

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,5 +1,5 @@
 export async function getData(){
-    const response = await fetch("http://localhost:3000/api")
+    const response = await fetch("https://video-club-grupo3.netlify.app/api")
     const json = await response.json()
     return json.map(movie => {
         const date = movie.date && movie.date.replaceAll("-","/")
